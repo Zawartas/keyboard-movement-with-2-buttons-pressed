2b67b2044c3118453e796a8c04a01260c311149c
Szymon <32157216+Zawartas@users.noreply.github.com>
2017-10-02 16:46:49 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,70 +6,83 @@ using namespace std;
 
 void button1(bool & pressedUP, bool & pressedRIGHT)
 {
+        pressedUP = false;
+    pressedRIGHT = false;
     unsigned char button;
 
     button = getch();
-    if (button = 224)
-        button = getch();
-
-    if (button == 72)
+    if (button == 224)
     {
-        pressedUP = true;
-        cout << "First thread: UP\n";
+        button = getch();
+        if (button == 72)
+        {
+            pressedUP = true;
+            cout << "First thread: UP\n";
+        }
+        else if (button == 77)
+        {
+            pressedRIGHT = true;
+            cout << "First thread: RIGHT\n";
+        }
     }
-    else if (button == 77)
-     {
-        pressedRIGHT = true;
-        cout << "First thread: RIGHT\n";
-    }
-    else
-        cout <<"First thread: Other button\n";
+    //else
+        //cout <<"First thread: Other button\n";
 }
 
 void button2(bool & pressedUP, bool & pressedRIGHT)
 {
+        pressedUP = false;
+    pressedRIGHT = false;
     unsigned char button;
 
     button = getch();
-    if (button == 72)
+
+    if (button == 224)
     {
-        pressedUP = true;
-        cout << "Second thread: UP\n";
+        button = getch();
+        if (button == 72)
+        {
+            pressedUP = true;
+            cout << "Second thread: UP\n";
+        }
+        else if (button == 77)
+        {
+            pressedRIGHT = true;
+            cout << "Second thread: RIGHT\n";
+        }
     }
-    else if (button == 77)
-     {
-        pressedRIGHT = true;
-        cout << "Second thread: RIGHT\n";
-    }
-    else
-        cout <<"Second thread: Other button\n";
+   // else
+        //cout <<"Second thread: Other button\n";
 }
 
-void buttonLEFT(bool & pressedLEFT)
-{
+void sensor (bool & gameIsRunning, bool & pressedUP, bool & pressedRIGHT){
 
-}
-
-void buttonDOWN(bool & pressedDOWN)
-{
+    while (gameIsRunning)
+    {
+        if (pressedRIGHT == true && pressedUP == true)
+            cout << "COMBO!!!" << endl;
+    }
 
 }
 
 int main()
 {
+    bool pressedUP;
+    bool pressedRIGHT;
+    bool gameIsRunning = true;
+
+    thread (sensor, ref(gameIsRunning), ref(pressedUP), ref(pressedRIGHT)).detach();
+
     do
     {
-    bool pressedUP = false;
-    bool pressedRIGHT = false;
+
 
     thread first (button1, ref(pressedUP), ref(pressedRIGHT));
     thread second (button2, ref(pressedUP), ref(pressedRIGHT));
 
         first.join();
         second.join();
-        // moze 3 watek z timerem i jesli nacisniecie nie pojawi sie w ciagu sekundy to bedzie konczyc cykl ?
-        if (pressedUP && pressedRIGHT)
-            cout << "\nUP_RIGHT\n";
+
     }
     while (1);
 
