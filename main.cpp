15b99dbafc6a3dc18727e57c9df6145075805523
Szymon <32157216+Zawartas@users.noreply.github.com>
2018-01-30 16:49:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,90 +1,64 @@
 #include <iostream>
 #include <thread>
 #include <conio.h>
+#include <ctime>
 
 using namespace std;
+void wait (int);
+void signal(int&);
 
-void button1(bool & pressedUP, bool & pressedRIGHT)
-{
-        pressedUP = false;
-    pressedRIGHT = false;
-    unsigned char button;
+int main(){
+//    bool gameIsRunning = true;
+    static int j = 0;
+//    static queue<int> kolejka;
 
-    button = getch();
-    if (button == 224)
-    {
-        button = getch();
-        if (button == 72)
-        {
-            pressedUP = true;
-            cout << "First thread: UP\n";
-        }
-        else if (button == 77)
-        {
-            pressedRIGHT = true;
-            cout << "First thread: RIGHT\n";
-        }
-    }
-    //else
-        //cout <<"First thread: Other button\n";
-}
+thread first (signal, ref(j));
+thread second (signal, ref(j));
 
-void button2(bool & pressedUP, bool & pressedRIGHT)
-{
-        pressedUP = false;
-    pressedRIGHT = false;
-    unsigned char button;
-
-    button = getch();
-
-    if (button == 224)
-    {
-        button = getch();
-        if (button == 72)
-        {
-            pressedUP = true;
-            cout << "Second thread: UP\n";
-        }
-        else if (button == 77)
-        {
-            pressedRIGHT = true;
-            cout << "Second thread: RIGHT\n";
-        }
-    }
-   // else
-        //cout <<"Second thread: Other button\n";
-}
-
-void sensor (bool & gameIsRunning, bool & pressedUP, bool & pressedRIGHT){
-
-    while (gameIsRunning)
-    {
-        if (pressedRIGHT == true && pressedUP == true)
-            cout << "COMBO!!!" << endl;
-    }
-
-}
-
-int main()
-{
-    bool pressedUP;
-    bool pressedRIGHT;
-    bool gameIsRunning = true;
-
-    thread (sensor, ref(gameIsRunning), ref(pressedUP), ref(pressedRIGHT)).detach();
-
-    do
-    {
-
-
-    thread first (button1, ref(pressedUP), ref(pressedRIGHT));
-    thread second (button2, ref(pressedUP), ref(pressedRIGHT));
-
-        first.join();
-        second.join();
-
-    }
-    while (1);
-
+first.join();
+second.join();
+        
     return 0;
 }
+
+
+void wait (int i){
+    int start = clock();
+    while (clock() < start + i){}}
+
+void signal(int & j){
+    unsigned char key;
+
+    while (key != 'q'){
+        key = getch();
+        if (key == 224){
+            key = getch();
+            switch(key){
+                case 72:
+                    j += 4;
+                    break;
+                case 77:
+                    j += 5;
+                    break;
+                case 80:
+                    j += 7;
+                    break;
+                case 75:
+                    j += 10;
+                    break;
+            }
+        }
+    wait(25);
+
+    if (j == 4) {cout << "Up\n"; j = 0;}
+    else if (j == 5) {cout << "Right\n"; j = 0;}
+    else if (j == 7) {cout << "Down\n"; j = 0;}
+    else if (j == 10) {cout << "Left\n"; j = 0;}
+    else if (j == 9) {cout << "Up_Right\n"; j = 0;}
+    else if (j == 11) {cout << "Up_Down\n"; j = 0;}
+    else if (j == 14) {cout << "Up_Left\n"; j = 0;}
+    else if (j == 12) {cout << "Down_Right\n"; j = 0;}
+    else if (j == 15) {cout << "Right_Left\n"; j = 0;}
+    else if (j == 17) {cout << "Down_Left\n"; j = 0;}
+
+}}
